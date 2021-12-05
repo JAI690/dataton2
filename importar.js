6d7e23a7c48669025af1855e3d7b98c3bbd90a77
Ele Cabrera <jai690@hotmail.com>
2021-12-05 13:06:17 -0600

    codigo subir datos

---
+++ importar.js
@@ -0,0 +1,1 @@
+//mongoimport --uri "mongodb+srv://jai690:Lamaquina10@practice.pzjne.mongodb.net/corrupcion?ssl=true" --collection sanciones --drop --jsonArray --file src/public/Data/Sistema3Servidores.json
