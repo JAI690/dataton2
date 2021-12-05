e0b4ae1965b47cb281856c04782a311ea66daabc
Ele Cabrera <jai690@hotmail.com>
2021-12-05 15:37:33 -0600

    formato de funciones

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -1,10 +1,28 @@
 const helpers = {};
+const moment = require('moment');
 const hb = require('handlebars');
 
 const accounting = require("accounting");
 
+hb.registerHelper('status', function (value) {
+    if(value === '1'){
+        return true;
+    }else{
+        return false;
+    }
+    
+  });
+  
+hb.registerHelper('dateFormat', function (date, options) {
+      const formatToUse = (arguments[1] && arguments[1].hash && arguments[1].hash.format) || "DD/MM/YYYY"
+      return moment(date).format(formatToUse);
+  });
+
+
   hb.registerHelper('accountingFormat', function (value) {
     return accounting.formatMoney(value);
   });
+    
+    
 
-  module.exports = helpers;
+module.exports = helpers;
